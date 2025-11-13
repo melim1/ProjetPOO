26db36c19706874922733665ba3e83c3d04a8964
Merouani Souha <souha@Merouanis-MacBook-Air.local>
2025-11-13 10:13:45 +0100

    lajout de la class Pilulier

--- Pilulier.java
+++ Pilulier.java
@@ -1,3 +1,24 @@
-public class Pilulier{
-    
+public class Pilulier extends CapteurConnecte{
+private int dosesRestantes;
+  
+    public Pilulier(String id, String nom, int dosesInitiales) {
+        super(id, nom);
+        this.dosesRestantes = dosesInitiales;
+    }
+
+    public void mesurer() {
+        if (Math.random() < 0.3 && dosesRestantes > 0) {
+            dosesRestantes -= 1;
+        }
+        valeur = dosesRestantes;
+    }
+
+    public boolean verifierAlerte() {
+        return dosesRestantes <= 0; 
+    }
+
+    public String toString() {
+        return super.toString() + String.format(" [Doses restantes=%d]", dosesRestantes);
+    }
+
 }
